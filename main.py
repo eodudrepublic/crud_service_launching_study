93d578715d23b159a36829af2003413a878a537a
Daeyoung Kim <eodudrepublic@gmail.com>
2024-02-06 16:12:23 +0900

    3일차 : AWS 배포
    2/6 오후에 한 내용
    일단 코드 상에서 fastAPI의 루트에 맞게 기존에 "/index.html"로 되어있던 경로를 "./index"로 변경해줬음
    AWS -> EC2 -> ubuntu -> 필요한 모듈 설치(python, fastapi, uvicorn 등) -> 사용할 포트 열기 -> git clone -> cd (프로젝트 디렉토리) -> host 0.0.0.0 port 8000으로 실행 (uvicorn) -> (public IPv4 주소):8000으로 접속
    (배포하는 과정 영상으로 찍어놨으니 영상 참고

--- main.py
+++ main.py
@@ -5,27 +5,38 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+# fastAPI에 static file을 추가하는 방법 -> 'StaticFiles'
 app.mount("/css", StaticFiles(directory="templates/css"), name="css")
 app.mount("/images", StaticFiles(directory="templates/images"), name="images")
 app.mount("/js", StaticFiles(directory="templates/js"), name="js")
 
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/", response_class=HTMLResponse)
 async def read_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/index", response_class=HTMLResponse)
+async def read_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.get("/service", response_class=HTMLResponse)
 async def read_service(request: Request):
     return templates.TemplateResponse("service.html", {"request": request})
 
+
 @app.get("/contact", response_class=HTMLResponse)
 async def read_contact(request: Request):
     return templates.TemplateResponse("contact.html", {"request": request})
 
+
 @app.get("/about", response_class=HTMLResponse)
 async def read_about(request: Request):
     return templates.TemplateResponse("about.html", {"request": request})
+
 
 @app.get("/blog", response_class=HTMLResponse)
 async def read_blog(request: Request):
